c14c4a3b5b5ed1a5cc8ef089ebd6ad79af24ad39
AbdullahShahid7402 <abdullahshahid210@gmail.com>
2024-10-30 15:15:35 +0500

    Pannel Switching added

--- Assets/Scripts/MainMenu_Pannel_Controller.cs
+++ Assets/Scripts/MainMenu_Pannel_Controller.cs
@@ -33,17 +33,25 @@ public class MainMenu_Pannel_Controller : MonoBehaviour
     public void Settings_button()
     {
         // Settings Button Logic here
+        MainMenu_Pannel.SetActive(false);
+        Settings_Pannel.SetActive(true);
     }
     public void HowToPlay_button()
     {
         // How To Play Button Logic here
+        MainMenu_Pannel.SetActive(false);
+        HowToPlay_Pannel.SetActive(true);
     }
     public void Credits_button()
     {
         // Credits Button Logic here
+        MainMenu_Pannel.SetActive(false);
+        Credits_Pannel.SetActive(true);
     }
     public void BackToMenu_button()
     {
         // Back To Menu Button Logic here
+        // already logic exists in initialize pannels function... so might aswell just use it as it is
+        Initialize_Pannels();
     }
 }
