75556318d0f7f6ff1e96f0adf4d2dd8c0ea4cf27
AbdullahShahid7402 <abdullahshahid210@gmail.com>
2024-11-01 08:43:40 +0500

    Button Press Sound Added

--- Assets/Scripts/MainMenu_Pannel_Controller.cs
+++ Assets/Scripts/MainMenu_Pannel_Controller.cs
@@ -6,6 +6,8 @@ public class MainMenu_Pannel_Controller : MonoBehaviour
 {
     public GameObject MainMenu_Pannel, Settings_Pannel, HowToPlay_Pannel, Credits_Pannel;
     private AudioSource audio_source;
+    public GameObject button_audio_source;
+    private AudioSource button_audio;
     
     // Start is called before the first frame update
     void Start()
@@ -14,12 +16,14 @@ public class MainMenu_Pannel_Controller : MonoBehaviour
         Initialize_Pannels();
         audio_source = GetComponent<AudioSource>();
         audio_source.volume = AudioSettings.volume;
+        button_audio = button_audio_source.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         audio_source.volume = AudioSettings.volume * AudioSettings.Unmute_factor;
+        button_audio.volume = AudioSettings.volume;
     }
 
     // just call this in main menu to remove all player saved data
@@ -41,6 +45,7 @@ public class MainMenu_Pannel_Controller : MonoBehaviour
     {
         // Play Button Logic here
         SceneManager.LoadScene("Gameplay");
+        button_audio.Play();
     }
     public void Settings_button()
     {
@@ -49,6 +54,7 @@ public class MainMenu_Pannel_Controller : MonoBehaviour
         var anim1 = MainMenu_Pannel.GetComponent<Animator>();
         anim1.SetTrigger("Right Move");
         Settings_Pannel.SetActive(true);
+        button_audio.Play();
     }
     public void HowToPlay_button()
     {
@@ -57,6 +63,7 @@ public class MainMenu_Pannel_Controller : MonoBehaviour
         var anim1 = MainMenu_Pannel.GetComponent<Animator>();
         anim1.SetTrigger("Up Move");
         HowToPlay_Pannel.SetActive(true);
+        button_audio.Play();
     }
     public void Credits_button()
     {
@@ -65,21 +72,25 @@ public class MainMenu_Pannel_Controller : MonoBehaviour
         var anim1 = MainMenu_Pannel.GetComponent<Animator>();
         anim1.SetTrigger("Down Move");
         Credits_Pannel.SetActive(true);
-    }
-    public void Mute_Click()
-    {
-        AudioSettings.Unmute_factor = 1;
+        button_audio.Play();
     }
 
     public void Remove_HighScore_Click()
     {
         PlayerPrefs.SetInt("HighScore",0);
         PlayerPrefs.Save();
+        button_audio.Play();
+    }
+    public void Mute_Click()
+    {
+        AudioSettings.Unmute_factor = 1;
+        button_audio.Play();
     }
 
     public void Unmute_Click()
     {
         AudioSettings.Unmute_factor = 0;
+        button_audio.Play();
     }
     public void BackToMenu_button()
     {
@@ -95,5 +106,6 @@ public class MainMenu_Pannel_Controller : MonoBehaviour
         anim1 = Credits_Pannel.GetComponent<Animator>();
         anim1.SetTrigger("Go Back");
         Invoke("Initialize_Pannels",1f);
+        button_audio.Play();
     }
 }
